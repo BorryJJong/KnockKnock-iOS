aae722b03044e672c759bb41f30e80c5b9c8819a
feelsodev <swdoriz@gmail.com>
2022-03-06 15:30:48 +0900

    feat : UIView addSubview extension 추가

--- KnockKnock-iOS/Extension-Utilities/Foundation/Array+Ext.swift
+++ KnockKnock-iOS/Extension-Utilities/Foundation/Array+Ext.swift
@@ -5,4 +5,13 @@
 //  Created by sangwon yoon on 2022/03/06.
 //
 
-import Foundation
+import UIKit
+
+extension Array where Element: UIView {
+  func addSubViews(_ parentView: UIView) {
+    self.forEach{
+      parentView.addSubview($0)
+    }
+  }
+}
+
