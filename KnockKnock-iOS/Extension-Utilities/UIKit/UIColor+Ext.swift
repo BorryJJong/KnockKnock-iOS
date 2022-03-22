5ac35c6ad8c741a2e62ef18b48dd5bbd3ca65fd3
dy-yb <ydyelin@gmail.com>
2022-03-22 11:19:02 +0900

    chore: class -> static 변경

--- KnockKnock-iOS/Extension-Utilities/UIKit/UIColor+Ext.swift
+++ KnockKnock-iOS/Extension-Utilities/UIKit/UIColor+Ext.swift
@@ -8,49 +8,49 @@
 import UIKit
 
 extension UIColor {
-  class var gray10: UIColor? {
+  static var gray10: UIColor? {
     return UIColor(named: "gray10")
   }
-  class var gray20: UIColor? {
+  static var gray20: UIColor? {
     return UIColor(named: "gray20")
   }
-  class var gray30: UIColor? {
+  static var gray30: UIColor? {
     return UIColor(named: "gray30")
   }
-  class var gray40: UIColor? {
+  static var gray40: UIColor? {
     return UIColor(named: "gray40")
   }
-  class var gray50: UIColor? {
+  static var gray50: UIColor? {
     return UIColor(named: "gray50")
   }
-  class var gray60: UIColor? {
+  static var gray60: UIColor? {
     return UIColor(named: "gray60")
   }
-  class var gray70: UIColor? {
+  static var gray70: UIColor? {
     return UIColor(named: "gray70")
   }
-  class var gray80: UIColor? {
+  static var gray80: UIColor? {
     return UIColor(named: "gray80")
   }
-  class var gray90: UIColor? {
+  static var gray90: UIColor? {
     return UIColor(named: "gray90")
   }
-  class var green10: UIColor? {
+  static var green10: UIColor? {
     return UIColor(named: "green10")
   }
-  class var green20: UIColor? {
+  static var green20: UIColor? {
     return UIColor(named: "green20")
   }
-  class var green30: UIColor? {
+  static var green30: UIColor? {
     return UIColor(named: "green30")
   }
-  class var green40: UIColor? {
+  static var green40: UIColor? {
     return UIColor(named: "green40")
   }
-  class var green50: UIColor? {
+  static var green50: UIColor? {
     return UIColor(named: "green50")
   }
-  class var green60: UIColor? {
+  static var green60: UIColor? {
     return UIColor(named: "green60")
   }
 }
