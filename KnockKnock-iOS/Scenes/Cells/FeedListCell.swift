0f5480c7988fe4b8ea100f901e9f4cfec828992d
dy-yb <daye.elin.yoon@gmail.com>
2022-06-22 16:03:48 +0900

    feat: feed list view생성

---
+++ KnockKnock-iOS/Scenes/Cells/FeedListCell.swift
@@ -0,0 +1,12 @@
+//
+//  FeedListCell.swift
+//  KnockKnock-iOS
+//
+//  Created by Daye on 2022/06/22.
+//
+
+import UIKit
+
+class FeedListCell: BaseCollectionViewCell {
+  
+}
---
+++ KnockKnock-iOS/Scenes/Feed/FeedList/FeedListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  FeedListViewController.swift
+//  KnockKnock-iOS
+//
+//  Created by Daye on 2022/06/22.
+//
+
+import UIKit
+
+import Then
+
+class FeedListViewController: BaseViewController<FeedListView> {
+
+  // MARK: - Life Cycles
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func setupConfigure() {
+    self.containerView.feedListCollectionView.do {
+      $0.delegate = self
+      $0.dataSource = self
+      $0.registCell(type: FeedListCell.self)
+    }
+  }
+}
+
+// MARK: - Extensions
+
+extension FeedListViewController: UICollectionViewDataSource {
+  func collectionView(
+    _ collectionView: UICollectionView,
+    numberOfItemsInSection section: Int
+  ) -> Int {
+    return 10
+  }
+
+  func collectionView(
+    _ collectionView: UICollectionView,
+    cellForItemAt indexPath: IndexPath
+  ) -> UICollectionViewCell {
+    let cell = collectionView.dequeueCell(withType: FeedListCell.self, for: indexPath)
+    
+    return cell
+  }
+}
+
+extension FeedListViewController: UICollectionViewDelegateFlowLayout {
+
+}
