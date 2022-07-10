653c5f1945bb60828127b81d544e91d39e45785a
dy-yb <daye.elin.yoon@gmail.com>
2022-07-10 21:37:27 +0900

    chore: feed collectionView 분리(tag, feed)

--- KnockKnock-iOS/Scenes/Feed/FeedViewController.swift
+++ KnockKnock-iOS/Scenes/Feed/FeedViewController.swift
@@ -18,11 +18,11 @@ final class FeedViewController: BaseViewController<FeedView> {
 
   // MARK: - Enums
 
-  private enum FeedSection: Int {
+  private enum CollectionViewTag: Int {
     case tag = 0
     case feed = 1
 
-    static let allCases: [FeedSection] = [.tag, .feed]
+    static let allCases: [CollectionViewTag] = [.tag, .feed]
   }
 
   // MARK: - Properties
@@ -37,19 +37,32 @@ final class FeedViewController: BaseViewController<FeedView> {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.extendedLayoutIncludesOpaqueBars = true
     self.interactor?.fetchFeed()
     self.interactor?.getChallengeTitles()
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
   }
 
   override func setupConfigure() {
     self.navigationItem.searchController = containerView.searchBar
 
-    self.containerView.feedCollectionView.do {
+    self.containerView.tagCollectionView.do {
       $0.delegate = self
       $0.dataSource = self
       $0.registCell(type: TagCell.self)
+      $0.collectionViewLayout = self.containerView.tagCollectionViewLayout()
+      $0.tag = CollectionViewTag.tag.rawValue
+    }
+
+    self.containerView.feedCollectionView.do {
+      $0.delegate = self
+      $0.dataSource = self
       $0.registCell(type: FeedCell.self)
       $0.collectionViewLayout = self.containerView.feedCollectionViewLayout()
+      $0.tag = CollectionViewTag.feed.rawValue
     }
   }
 }
@@ -67,14 +80,14 @@ extension FeedViewController: FeedViewProtocol {
 
     if let index = index {
       UIView.performWithoutAnimation {
-        self.containerView.feedCollectionView.reloadSections([FeedSection.tag.rawValue])
-        self.containerView.feedCollectionView.scrollToItem(
+        self.containerView.tagCollectionView.reloadSections([0])
+        self.containerView.tagCollectionView.scrollToItem(
           at: index,
           at: .centeredHorizontally,
           animated: false)
       }
     } else {
-      self.containerView.feedCollectionView.reloadData()
+      self.containerView.tagCollectionView.reloadData()
     }
   }
 }
@@ -84,11 +97,11 @@ extension FeedViewController: UICollectionViewDataSource {
     _ collectionView: UICollectionView,
     numberOfItemsInSection section: Int
   ) -> Int {
-    switch section {
-    case FeedSection.tag.rawValue:
+    switch collectionView.tag {
+    case CollectionViewTag.tag.rawValue:
       return self.challengeTitles.count
 
-    case FeedSection.feed.rawValue:
+    case CollectionViewTag.feed.rawValue:
       return self.feed.count
       
     default:
@@ -96,16 +109,12 @@ extension FeedViewController: UICollectionViewDataSource {
     }
   }
 
-  func numberOfSections(in collectionView: UICollectionView) -> Int {
-    return FeedSection.allCases.count
-  }
-
   func collectionView(
     _ collectionView: UICollectionView,
     cellForItemAt indexPath: IndexPath
   ) -> UICollectionViewCell {
-    switch indexPath.section {
-    case FeedSection.tag.rawValue:
+    switch collectionView.tag {
+    case CollectionViewTag.tag.rawValue:
       let cell = collectionView.dequeueCell(withType: TagCell.self, for: indexPath)
       cell.bind(tag: self.challengeTitles[indexPath.item])
 
@@ -116,7 +125,7 @@ extension FeedViewController: UICollectionViewDataSource {
 
       return cell
 
-    case FeedSection.feed.rawValue:
+    case CollectionViewTag.feed.rawValue:
       let cell = collectionView.dequeueCell(withType: FeedCell.self, for: indexPath)
       let feed = self.feed[indexPath.item]
       cell.bind(feed: feed)
@@ -142,14 +151,14 @@ extension FeedViewController: UICollectionViewDelegate {
     _ collectionView: UICollectionView,
     didSelectItemAt indexPath: IndexPath
   ) {
-    switch indexPath.section {
-    case FeedSection.tag.rawValue:
+    switch collectionView.tag {
+    case CollectionViewTag.tag.rawValue:
       self.interactor?.setSelectedStatus(
         challengeTitles: challengeTitles,
         selectedIndex: indexPath
       )
 
-    case FeedSection.feed.rawValue:
+    case CollectionViewTag.feed.rawValue:
       self.router.navigateToFeedList(
         source: self,
         destination: FeedListRouter.createFeedList() as! FeedListViewController
