5bca8b61e4edadd5609cbdc6470d1cd69f932fb0
dy-yb <daye.elin.yoon@gmail.com>
2022-07-19 21:28:01 +0900

    feat: 답글있을 경우 활성화 버튼 추가

--- KnockKnock-iOS/Scenes/Cells/CommentCell.swift
+++ KnockKnock-iOS/Scenes/Cells/CommentCell.swift
@@ -11,6 +11,10 @@ import KKDSKit
 
 class CommentCell: BaseCollectionViewCell {
 
+  // MARK: - Properties
+
+  let countReplies: Int = 3
+
   // MARK: - UIs
 
   private let profileImageView = UIImageView().then {
@@ -38,17 +42,30 @@ class CommentCell: BaseCollectionViewCell {
     $0.textColor = .gray70
   }
 
-  private let replyButton = UIButton().then {
+  private let replyWriteButton = UIButton().then {
     $0.translatesAutoresizingMaskIntoConstraints = false
     $0.setTitleColor(.gray70, for: .normal)
-    $0.setImage(KKDS.Image.etc_reply_8_gr, for: .normal)
+    $0.setImage(KKDS.Image.etc_bar_8_gr, for: .normal)
     $0.setTitle("   댓글달기", for: .normal)
     $0.semanticContentAttribute = .forceLeftToRight
     $0.titleLabel?.font = .systemFont(ofSize: 12, weight: .semibold)
   }
 
+  lazy var replyMoreButton = UIButton().then {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.setImage(KKDS.Image.etc_bar_30_gr, for: .normal)
+    $0.setTitle("   답글 \(self.countReplies)개 보기", for: .normal)
+    $0.setTitle("답글 숨기기", for: .selected)
+    $0.setTitleColor(.gray70, for: .normal)
+    $0.titleLabel?.font = .systemFont(ofSize: 12, weight: .bold)
+    $0.isHidden = self.countReplies == 0
+  }
+
+  @objc private func replyMoreButtonDidTap(_ sender: UIButton) {
+  }
+
   override func setupConstraints() {
-    [self.profileImageView, self.userIdLabel, self.commentLabel, self.writtenDateLabel, self.replyButton].addSubViews(self.contentView)
+    [self.profileImageView, self.userIdLabel, self.commentLabel, self.writtenDateLabel, self.replyWriteButton, self.replyMoreButton].addSubViews(self.contentView)
 
     NSLayoutConstraint.activate([
       self.profileImageView.topAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.topAnchor),
@@ -64,13 +81,16 @@ class CommentCell: BaseCollectionViewCell {
       self.commentLabel.leadingAnchor.constraint(equalTo: self.profileImageView.trailingAnchor, constant: 10),
       self.commentLabel.trailingAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.trailingAnchor),
 
+      self.replyMoreButton.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+      self.replyMoreButton.leadingAnchor.constraint(equalTo: self.commentLabel.leadingAnchor),
+      self.replyMoreButton.topAnchor.constraint(equalTo: self.writtenDateLabel.bottomAnchor, constant: 15),
+
       self.writtenDateLabel.topAnchor.constraint(equalTo: self.commentLabel.bottomAnchor, constant: 3),
       self.writtenDateLabel.leadingAnchor.constraint(equalTo: self.commentLabel.leadingAnchor),
-      self.writtenDateLabel.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
 
-      self.replyButton.leadingAnchor.constraint(equalTo: self.writtenDateLabel.trailingAnchor, constant: 10),
-      self.replyButton.topAnchor.constraint(equalTo: self.writtenDateLabel.topAnchor),
-      self.replyButton.bottomAnchor.constraint(equalTo: self.writtenDateLabel.bottomAnchor)
+      self.replyWriteButton.leadingAnchor.constraint(equalTo: self.writtenDateLabel.trailingAnchor, constant: 10),
+      self.replyWriteButton.topAnchor.constraint(equalTo: self.writtenDateLabel.topAnchor),
+      self.replyWriteButton.bottomAnchor.constraint(equalTo: self.writtenDateLabel.bottomAnchor)
     ])
   }
 }
