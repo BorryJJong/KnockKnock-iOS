752ca998b617405ec2e95f9ec5e46a53b1ccc7d3
dy-yb <daye.elin.yoon@gmail.com>
2022-08-10 12:13:48 +0900

    feat: home main section pager ui, action

--- KnockKnock-iOS/Scenes/Cells/Home/HomeMainPagerCell.swift
+++ KnockKnock-iOS/Scenes/Cells/Home/HomeMainPagerCell.swift
@@ -13,10 +13,20 @@ import Then
 
 class HomeMainPagerCell: BaseCollectionViewCell {
 
+  // MARK: - Constants
+
+  private enum Metric {
+    static let lineBackgroundViewWidth = 245.f
+    static let lineBackgroundViewHeight = 2.f
+
+    static let lineBackgroundViewLeadingMargin = 20.f
+    static let lineBackgroundViewBottomMargin = -30.f
+  }
+
   // MARK: - Properties
 
-  var mainImages: [String] = ["", "", ""]
-  var page: Int?
+  var mainImages: [String] = ["", "", "", "", "", ""]
+  var currentIndex: CGFloat = 0
 
   // MARK: - UIs
 
@@ -36,6 +46,19 @@ class HomeMainPagerCell: BaseCollectionViewCell {
     $0.backgroundStyle = .minimal
   }
 
+  let lineView = UIView().then {
+    $0.backgroundColor = .white
+  }
+
+  let lineBackgroundView = UIView().then {
+    $0.backgroundColor = UIColor(
+      red: 255/255,
+      green: 255/255,
+      blue: 255/255,
+      alpha: 0.3
+    )
+  }
+
   override func setupCollectionView() {
     self.homeMainCollectionView.do {
       $0.delegate = self
@@ -43,21 +66,26 @@ class HomeMainPagerCell: BaseCollectionViewCell {
       $0.registCell(type: HomeMainCell.self)
       $0.collectionViewLayout = self.setHomeMainPagerCollectionViewLayout()
     }
-    self.pageControl.do {
-      $0.numberOfPages = self.mainImages.count
-      $0.setIndicatorImage(KKDS.Image.ic_page_10_wh_on, forPage: $0.currentPage)
-    }
   }
 
   override func setupConstraints() {
-    [self.homeMainCollectionView, self.pageControl].addSubViews(self.contentView)
+    [self.homeMainCollectionView, self.lineBackgroundView].addSubViews(self.contentView)
+    [self.lineView].addSubViews(self.lineBackgroundView)
 
     self.homeMainCollectionView.snp.makeConstraints {
       $0.edges.equalTo(self.contentView)
     }
 
-    self.pageControl.snp.makeConstraints {
-      $0.bottom.leading.equalTo(self.contentView).inset(20)
+    self.lineView.snp.makeConstraints {
+      $0.top.bottom.leading.equalToSuperview()
+      $0.width.equalTo(Metric.lineBackgroundViewWidth / CGFloat(self.mainImages.count))
+    }
+
+    self.lineBackgroundView.snp.makeConstraints {
+      $0.leading.equalTo(self.contentView).offset(Metric.lineBackgroundViewLeadingMargin)
+      $0.bottom.equalTo(self.contentView).offset(Metric.lineBackgroundViewBottomMargin)
+      $0.width.equalTo(Metric.lineBackgroundViewWidth)
+      $0.height.equalTo(Metric.lineBackgroundViewHeight)
     }
   }
 
@@ -80,37 +108,47 @@ class HomeMainPagerCell: BaseCollectionViewCell {
     let mainSection = NSCollectionLayoutSection(group: mainGroup)
     mainSection.orthogonalScrollingBehavior = .paging
 
+    mainSection.visibleItemsInvalidationHandler = ({ (visibleItems, point, env) in
+      self.currentIndex = round(point.x / UIScreen.main.bounds.width)
+      self.movePager(currentPage: self.currentIndex, itemCount: self.mainImages.count)
+    })
+
     let layout = UICollectionViewCompositionalLayout(section: mainSection)
 
     return layout
   }
 
+  func movePager(currentPage: CGFloat, itemCount: Int) {
+    UIView.animate(withDuration: 0.5, animations: {
+      let itemCount = CGFloat(itemCount)
+      self.lineView.transform = CGAffineTransform(
+        translationX: CGFloat(Metric.lineBackgroundViewWidth/itemCount) * currentPage,
+        y: 0
+      )
+    })
+  }
 }
 
+// MARK: - CollectionView DataSource, Delegate
+
 extension HomeMainPagerCell: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+  func collectionView(
+    _ collectionView: UICollectionView,
+    numberOfItemsInSection section: Int
+  ) -> Int {
     return self.mainImages.count
   }
 
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueCell(withType: HomeMainCell.self, for: indexPath)
+  func collectionView(
+    _ collectionView: UICollectionView,
+    cellForItemAt indexPath: IndexPath
+  ) -> UICollectionViewCell {
+
+    let cell = collectionView.dequeueCell(
+      withType: HomeMainCell.self,
+      for: indexPath
+    )
 
     return cell
-  }
-
-  func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-    self.page = indexPath.item
-  }
-
-  func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-    if indexPath.item == self.page {
-      page = nil
-    }
-
-    if indexPath.item == self.pageControl.currentPage {
-      if let page = self.page {
-        self.pageControl.currentPage = page
-      }
-    }
   }
 }
