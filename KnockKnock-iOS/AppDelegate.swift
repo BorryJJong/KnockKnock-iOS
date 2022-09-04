5884254729005da965b91d7cd13a66295dc57369
dy-yb <daye.elin.yoon@gmail.com>
2022-09-04 14:20:11 +0900

    feat: Firebase sdk 설정

--- KnockKnock-iOS/AppDelegate.swift
+++ KnockKnock-iOS/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    FirebaseApp.configure()
+
     return true
   }
 
