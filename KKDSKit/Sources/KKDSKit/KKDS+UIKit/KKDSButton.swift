9ab878ef35bb34cf7e57206ecd4958f750118648
dy-yb <daye.elin.yoon@gmail.com>
2022-10-10 13:11:27 +0900

    feat: 줄바꿈처리

--- KKDSKit/Sources/KKDSKit/KKDS+UIKit/KKDSButton.swift
+++ KKDSKit/Sources/KKDSKit/KKDS+UIKit/KKDSButton.swift
@@ -21,10 +21,23 @@ public final class MiddleButton: UIButton {
   private func configure(title: String) {
     self.setTitle(title, for: .normal)
     self.setTitleColor(
-      UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1), for: .normal)
+      UIColor(
+        red: 85/255,
+        green: 85/255,
+        blue: 85/255,
+        alpha: 1
+      ),
+      for: .normal
+    )
     self.titleLabel?.font = .systemFont(ofSize: 13, weight: .medium)
+    
     self.layer.borderWidth = 1
-    self.layer.borderColor = UIColor(red: 211/255, green: 211/255, blue: 211/255, alpha: 1).cgColor
+    self.layer.borderColor = UIColor(
+      red: 211/255,
+      green: 211/255,
+      blue: 211/255,
+      alpha: 1)
+    .cgColor
     self.layer.cornerRadius = 3
   }
 }
