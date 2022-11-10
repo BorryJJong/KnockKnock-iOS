3ceb4b9e9ee6b1730b7fe95df271c01e920179a6
dy-yb <daye.elin.yoon@gmail.com>
2022-11-10 20:53:54 +0900

    feat: table header 추가

--- KnockKnock-iOS/Scenes/Feed/FeedWrite/Property/Views/PropertySelectView.swift
+++ KnockKnock-iOS/Scenes/Feed/FeedWrite/Property/Views/PropertySelectView.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 import Then
+import KKDSKit
+import SnapKit
 
 final class PropertySelectView: UIView {
 
@@ -21,10 +23,21 @@ final class PropertySelectView: UIView {
     $0.allowsMultipleSelection = true
   }
 
+  let propertyTableHeaderView = UIView().then {
+    $0.backgroundColor = KKDS.Color.gray10
+  }
+
+  let propertyHeaderLabel = UILabel().then {
+    $0.text = "최대 5개까지 선택 가능합니다."
+    $0.textColor = KKDS.Color.gray60
+    $0.font = .systemFont(ofSize: 12, weight: .semibold)
+  }
+
   // MARK: - Initialize
 
   init() {
     super.init(frame: .zero)
+    self.configure()
     self.setupConstraints()
   }
 
@@ -32,16 +45,30 @@ final class PropertySelectView: UIView {
     super.init(coder: aDecoder)
   }
 
+  private func configure() {
+    self.propertyTableHeaderView.frame = CGRect(
+      x: 0,
+      y: 0,
+      width: self.frame.width,
+      height: 50
+    )
+
+    self.propertyTableView.tableHeaderView = self.propertyTableHeaderView
+  }
+
   // MARK: - Constraints
 
   private func setupConstraints() {
     [self.propertyTableView].addSubViews(self)
+    [self.propertyHeaderLabel].addSubViews(self.propertyTableHeaderView)
 
-    NSLayoutConstraint.activate([
-      self.propertyTableView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
-      self.propertyTableView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-      self.propertyTableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-      self.propertyTableView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
-    ])
+    self.propertyHeaderLabel.snp.makeConstraints {
+      $0.leading.equalToSuperview().offset(20)
+      $0.centerY.equalToSuperview()
+    }
+
+    self.propertyTableView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
   }
 }
