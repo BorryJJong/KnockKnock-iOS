9a03b53db25fe44488d98af3fcd4dd74f6b0702a
dy-yb <daye.elin.yoon@gmail.com>
2022-12-20 17:14:56 +0900

    feat: apple login manager 작성

--- KnockKnock-iOS/Repository/AccountManager.swift
+++ KnockKnock-iOS/Repository/AccountManager.swift
@@ -27,6 +27,74 @@ protocol AccountManagerProtocol {
   func signOut(completionHanlder: @escaping (Bool) -> Void)
 }
 
+final class AppleAccountManager: SocialLoginManagerProtocol {
+
+  // 회원가입
+
+  func signUp(
+    loginInfo: LoginInfo,
+    nickname: String,
+    image: String,
+    completionHandler: @escaping (SignUpResponse) -> Void
+  ) {
+    let parameters = [
+      "socialUuid": loginInfo.socialUuid,
+      "socialType": loginInfo.socialType,
+      "nickname": nickname,
+      "image": image
+    ]
+
+    KKNetworkManager
+      .shared
+      .request(
+        object: SignUpResponse.self,
+        router: KKRouter.postSignUp(userInfo: parameters),
+        success: { success in
+          completionHandler(success)
+        }, failure: { error in
+          print(error)
+        }
+      )
+  }
+
+  // 로그인
+
+  func requestToken(
+    completionHandler: @escaping (LoginResponse, LoginInfo) -> Void
+  ) {
+    self.loginWithApple(completionHandler: { accessToken in
+
+      let loginInfo = LoginInfo(
+        socialUuid: accessToken,
+        socialType: SocialType.apple.rawValue
+      )
+
+      let parameters = [
+        "socialUuid": accessToken,
+        "socialType": SocialType.apple.rawValue
+      ]
+
+      KKNetworkManager
+        .shared
+        .request(
+          object: LoginResponse.self,
+          router: KKRouter.postSocialLogin(loginInfo: parameters),
+          success: { response in
+            completionHandler(response, loginInfo)
+          },
+          failure: { error in
+            print(error)
+          }
+        )
+    })
+  }
+
+  func loginWithApple(completionHandler: @escaping (String) -> Void) {
+    // login with apple
+  }
+
+}
+
 final class KakaoAccountManager: SocialLoginManagerProtocol {
 
   // 회원가입
@@ -63,31 +131,31 @@ final class KakaoAccountManager: SocialLoginManagerProtocol {
   func requestToken(
     completionHandler: @escaping (LoginResponse, LoginInfo) -> Void
   ) {
-      self.loginWithKakao(completionHandler: { accessToken in
+    self.loginWithKakao(completionHandler: { accessToken in
 
-        let loginInfo = LoginInfo(
-          socialUuid: accessToken,
-          socialType: SocialType.kakao.rawValue
+      let loginInfo = LoginInfo(
+        socialUuid: accessToken,
+        socialType: SocialType.kakao.rawValue
+      )
+
+      let parameters = [
+        "socialUuid": accessToken,
+        "socialType": SocialType.kakao.rawValue
+      ]
+
+      KKNetworkManager
+        .shared
+        .request(
+          object: LoginResponse.self,
+          router: KKRouter.postSocialLogin(loginInfo: parameters),
+          success: { response in
+            completionHandler(response, loginInfo)
+          },
+          failure: { error in
+            print(error)
+          }
         )
-
-        let parameters = [
-          "socialUuid": accessToken,
-          "socialType": SocialType.kakao.rawValue
-        ]
-
-        KKNetworkManager
-          .shared
-          .request(
-            object: LoginResponse.self,
-            router: KKRouter.postSocialLogin(loginInfo: parameters),
-            success: { response in
-              completionHandler(response, loginInfo)
-            },
-            failure: { error in
-              print(error)
-            }
-          )
-      })
+    })
   }
 
   func loginWithKakao(completionHandler: @escaping (String) -> Void) {
