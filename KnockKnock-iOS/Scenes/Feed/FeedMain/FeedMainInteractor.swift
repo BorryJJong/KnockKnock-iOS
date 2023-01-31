95dd7551522c0713efa02af638a2924694e7c432
dy-yb <daye.elin.yoon@gmail.com>
2023-01-31 15:28:22 +0900

    feat: 피드 메인 삭제 반영

--- KnockKnock-iOS/Scenes/Feed/FeedMain/FeedMainInteractor.swift
+++ KnockKnock-iOS/Scenes/Feed/FeedMain/FeedMainInteractor.swift
@@ -27,6 +27,14 @@ final class FeedMainInteractor: FeedMainInteractorProtocol {
 
   private var feedData: FeedMain?
 
+  // MARK: - Initialize
+
+  init() {
+    self.setNotification()
+  }
+
+  // MARK: - Business Logic
+
   func saveSearchKeyword(searchKeyword: [SearchKeyword]) {
     self.worker?.saveSearchKeyword(searchKeyword: searchKeyword)
   }
@@ -77,5 +85,36 @@ final class FeedMainInteractor: FeedMainInteractorProtocol {
       }
     }
     presenter?.presentGetChallengeTitles(challengeTitle: challengeTitles, index: selectedIndex)
+  }
+}
+
+extension FeedMainInteractor {
+
+  /// 피드 상세에서 발생한 삭제 이벤트 반영
+  @objc
+  private func deleteNotificationEvent(_ notification: Notification) {
+    guard let feedId = notification.object as? Int else { return }
+
+    guard let feedData = self.feedData else { return }
+    guard !feedData.feeds.isEmpty else { return }
+
+    // 삭제 요청한 피드의 index
+    guard let index = self.feedData?.feeds.firstIndex(where: { feedId == $0.id }) else { return }
+
+    self.feedData?.feeds.remove(at: index)
+
+    guard let feedData = self.feedData else { return }
+    self.presenter?.presentFeedMain(feed: feedData)
+  }
+
+  // MARK: - Notification Center
+
+  private func setNotification() {
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(self.deleteNotificationEvent(_:)),
+      name: .feedMainRefreshAfterDelete,
+      object: nil
+    )
   }
 }
