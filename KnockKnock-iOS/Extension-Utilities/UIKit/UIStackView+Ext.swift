2f32c75342965767abdb5b2e8f930f6515301266
dy-yb <daye.elin.yoon@gmail.com>
2023-02-02 16:00:42 +0900

    chore: 불필요 코드 삭제

--- KnockKnock-iOS/Extension-Utilities/UIKit/UIStackView+Ext.swift
+++ KnockKnock-iOS/Extension-Utilities/UIKit/UIStackView+Ext.swift
@@ -26,33 +26,29 @@ extension UIStackView {
       images.append(nil)
     }
 
-    Task {
-      do {
-        for index in 0..<images.count {
-          let imageView = UIImageView().then {
-            $0.contentMode = .scaleAspectFill
-            $0.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
-            $0.layer.cornerRadius = $0.frame.width / 2
-            $0.layer.borderWidth = 1
-            $0.layer.borderColor = UIColor.white.cgColor
-            $0.backgroundColor = .white
-            $0.clipsToBounds = true
-          }
+    for index in 0..<images.count {
+      let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        $0.layer.cornerRadius = $0.frame.width / 2
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.white.cgColor
+        $0.backgroundColor = .white
+        $0.clipsToBounds = true
+      }
 
-          let profileImage = images[index]
-            .flatMap { URL(string: $0) }
-            .flatMap { try? Data(contentsOf: $0) }
-            .flatMap { UIImage(data: $0) }
-          ?? KKDS.Image.ic_person_24
+      let profileImage = images[index]
+        .flatMap { URL(string: $0) }
+        .flatMap { try? Data(contentsOf: $0) }
+        .flatMap { UIImage(data: $0) }
+      ?? KKDS.Image.ic_person_24
 
-          imageView.image = await profileImage.resizeSquareImage(newWidth: 24)
+      imageView.image = profileImage.resizeSquareImage(newWidth: 24)
 
-          addArrangedSubview(imageView)
+      addArrangedSubview(imageView)
 
-          if index == 2 {
-            break
-          }
-        }
+      if index == 2 {
+        break
       }
     }
   }
