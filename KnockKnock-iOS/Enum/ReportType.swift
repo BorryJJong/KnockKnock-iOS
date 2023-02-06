ac91d40206d2bf3e01afc157a0cf8a08534df046
dy-yb <daye.elin.yoon@gmail.com>
2023-02-06 18:39:56 +0900

    feat: reportView ui layout 구성

---
+++ KnockKnock-iOS/Enum/ReportType.swift
@@ -0,0 +1,28 @@
+//
+//  ReportType.swift
+//  
+//
+//  Created by Daye on 2023/02/06.
+//
+
+import Foundation
+
+enum ReportType: String, CaseIterable {
+  case inappropriateContent = "INAPPROPRIATE_CONTENT"
+  case unauthorizeUse = "UNAUTHORIZED_USE"
+  case personalInfoExtrusion = "PERSONAL_INFORMATION_EXTRUSION"
+
+  var message: String {
+    switch self {
+
+    case .inappropriateContent:
+      return "음란/욕설/비방 등 부적절한 내용"
+
+    case .unauthorizeUse:
+      return "저작권 및 초상권 무단 도용"
+
+    case .personalInfoExtrusion:
+      return "개인정보 유출"
+    }
+  }
+}
