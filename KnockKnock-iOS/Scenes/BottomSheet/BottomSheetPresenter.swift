2e105baa6d0fd858178861de7abd74c1365a89f8
윤다예 <daye.elin.yoon@gmail.com>
2023-02-26 16:00:23 +0900

    feat: presenter 생성

---
+++ KnockKnock-iOS/Scenes/BottomSheet/BottomSheetPresenter.swift
@@ -0,0 +1,22 @@
+//
+//  BottomSheetPresenter.swift
+//  KnockKnock-iOS
+//
+//  Created by Daye on 2023/02/26.
+//
+
+import Foundation
+
+final class BottomSheetPresenter: BottomSheetPresenterProtocol {
+  var view: BottomSheetViewProtocol?
+
+  func presentOptions(
+    options: [String],
+    bottomSheetType: BottomSheetType
+  ) {
+    self.view?.fetchOptions(
+      options: options,
+      bottomSheetType: bottomSheetType
+    )
+  }
+}
