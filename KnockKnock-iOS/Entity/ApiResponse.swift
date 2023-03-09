887b9b7557ba33f49faf9e43d967890e15416634
윤다예 <daye.elin.yoon@gmail.com>
2023-03-09 14:59:30 +0900

    chore: 주석 수정

--- KnockKnock-iOS/Entity/ApiResponse.swift
+++ KnockKnock-iOS/Entity/ApiResponse.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /// API 응답 값 default 구조
 /// - code: status code(e.g. 200, 400)
-/// - message: 성공 여부 (e.g. SUCCESS or FAIL)
+/// - message: 에러 메세지 (e.g. Unauthorized)
 /// - data: 데이터 부분
 struct ApiResponseDTO<T: Decodable>: Decodable {
   let code: Int
