81285bd9c8aa627c26ec9416ffb9bf11a7a29f91
윤다예 <daye.elin.yoon@gmail.com>
2023-03-12 19:09:28 +0900

    feat: store, eventDetail entity definition

--- KnockKnock-iOS/Entity/Home/Store.swift
+++ KnockKnock-iOS/Entity/Home/Store.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct StoreDTO: Decodable {
+  let name: String
+  let description: String
+  let image: String
+  let shopPromotionNames: [String]
+}
+
+struct Store: Decodable {
+  let name: String
+  let description: String
+  let image: String
+  let shopPromotionNames: [String]
+}
+
+extension StoreDTO {
+  func toDomain() -> Store {
+    return .init(
+      name: name,
+      description: description,
+      image: image,
+      shopPromotionNames: shopPromotionNames
+    )
+  }
+}
