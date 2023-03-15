da2ba248aa61141f5fa55174e2b5dc52b5e585ad
윤다예 <daye.elin.yoon@gmail.com>
2023-03-15 13:12:30 +0900

    feat: gradient imageview 삭제

--- KnockKnock-iOS/Scenes/Cells/Home/HomeMainCell.swift
+++ KnockKnock-iOS/Scenes/Cells/Home/HomeMainCell.swift
@@ -16,16 +16,7 @@ final class HomeMainCell: BaseCollectionViewCell {
   // MARK: - UIs
 
   private let mainImageView = UIImageView().then {
-    $0.image = UIImage(named: "feed_sample_1")
     $0.contentMode = .scaleToFill
-  }
-
-  private let topGradientImageView = UIImageView().then {
-    $0.image = KKDS.Image.ic_bg_gradient_top_bk
-  }
-
-  private let bottomGradientImageView = UIImageView().then {
-    $0.image = KKDS.Image.ic_bg_gradient_bottom_bk
   }
 
   // MARK: - Bind
@@ -40,18 +31,10 @@ final class HomeMainCell: BaseCollectionViewCell {
   // MARK: - Constraints
 
   override func setupConstraints() {
-    [self.mainImageView, self.topGradientImageView, self.bottomGradientImageView].addSubViews(self.contentView)
+    [self.mainImageView].addSubViews(self.contentView)
 
     self.mainImageView.snp.makeConstraints {
       $0.edges.equalTo(self.contentView)
-    }
-
-    self.topGradientImageView.snp.makeConstraints {
-      $0.top.trailing.leading.equalTo(self.contentView)
-    }
-
-    self.bottomGradientImageView.snp.makeConstraints {
-      $0.bottom.trailing.leading.equalTo(self.contentView)
     }
   }
 }
